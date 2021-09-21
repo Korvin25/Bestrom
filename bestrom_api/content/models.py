c68cd900632853ec7624d0782b886c57fcf8627c
bexram <bexram33@mail.ru>
2021-09-21 09:36:19 +0300

    content models issuse

--- bestrom_api/content/models.py
+++ bestrom_api/content/models.py
@@ -22,7 +22,7 @@ class Block(models.Model):
     page = models.ForeignKey(Page, on_delete=models.CASCADE, related_name='blocks')
 
     def __str__(self):
-        return self.name
+        return str(self.page.title)+' '+str(self.name)
 
     class Meta:
         verbose_name_plural = 'Блоки'
@@ -45,10 +45,10 @@ class Content(models.Model):
     name = models.CharField(verbose_name='Название', max_length=1024, null=True)
     block = models.ForeignKey(Block, on_delete=models.CASCADE,related_name='contents')
     file = models.ManyToManyField(ContFile, blank=True)
-    text = models.CharField(verbose_name='Содержимое', max_length=1024, null=True, blank=True)
+    text = models.TextField(verbose_name='Содержимое', null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return str(self.block.page.title)+' '+str(self.block.name)+' '+str(self.name)
 
     class Meta:
         verbose_name_plural = 'Контент'
@@ -95,8 +95,8 @@ class News(models.Model):
         return self.name
 
     class Meta:
-        verbose_name_plural = 'Партнеры'
-        verbose_name = 'Партнер'
+        verbose_name_plural = 'Новости'
+        verbose_name = 'Новость'
 
 
 class Vacancy(models.Model):
