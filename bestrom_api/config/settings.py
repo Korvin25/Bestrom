4bf21863d565d0c9f3ea1b1d10dfbdefebbd00a5
bexram <bexram33@mail.ru>
2021-09-21 10:31:29 +0300

    allowed host

--- bestrom_api/config/settings.py
+++ bestrom_api/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-723l$4@qf6ok^=a$o(_l-)=fij3pd$8%am3+$zvm06qw+eh+x0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
