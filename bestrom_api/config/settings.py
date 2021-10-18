57a33bdc399e85f4ddf7d2a2788298ed1fa9f6ff
Bexram <bexram33@mail.ru>
2021-10-18 20:18:02 +0300

    static in settings

--- bestrom_api/config/settings.py
+++ bestrom_api/config/settings.py
@@ -14,6 +14,13 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+
+MEDIA_URL = "/media/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+STATIC_URL = "/static/"
 
 
 # Quick-start development settings - unsuitable for production
