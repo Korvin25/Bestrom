2145a3bf23c0207a9f40117098dee809d55da46f
bexram <bexram33@mail.ru>
2021-10-22 16:08:27 +0300

    APi issuse

--- bestrom_api/content/urls.py
+++ bestrom_api/content/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('page/<int:pk>/', views.GetContent.as_view()),
     path('news/', views.GetNews.as_view()),
-    path('client/', views.GetNews.as_view()),
-    path('partner/', views.GetNews.as_view()),
+    path('client/', views.GetClient.as_view()),
+    path('partner/', views.GetPartner.as_view()),
     path('vacancy/', views.GetNews.as_view()),
 ]
