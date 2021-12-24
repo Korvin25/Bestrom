300e303f282d505e0c4678e31bc9aad918685e43
bexram <bexram33@mail.ru>
2021-12-24 09:15:28 +0300

    add files to email

--- bestrom_api/crm/models.py
+++ bestrom_api/crm/models.py
@@ -1,28 +1,40 @@
 import datetime
 
-from django.db import models
-from django.core.mail import send_mail
 from config import settings
-
+from django.core.mail import EmailMessage
+from django.db import models
 # Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
 class Crm_forms(models.Model):
     dateapp = models.DateTimeField(auto_now=True, verbose_name='Дата поступления')
     type = models.CharField(verbose_name='Тип заявки', max_length=100, null=True, blank=True)
     telephone = models.CharField(verbose_name='Номер телефона', max_length=20, null=True, blank=True)
     email = models.CharField(verbose_name='Почта', max_length=50, null=True, blank=True)
     name = models.CharField(verbose_name='Обращение', max_length=50, null=True, blank=True)
-    other = models.TextField(verbose_name='Примечание',null=True, blank=True)
+    other = models.TextField(verbose_name='Примечание', null=True, blank=True)
     file = models.FileField(verbose_name='Файл', null=True, blank=True, upload_to='crm/')
 
     def __str__(self):
         return str(self.dateapp)
 
-    def save(self, *args, **kwargs):
-
-        send_mail('Заявка '+str(self.type)+' '+str(datetime.datetime.now()), str(self.name)+' '+str(self.telephone)+' '+str(self.email)+' '+str(self.other), settings.EMAIL_HOST_USER,
-                  ['ivan@adving.ru','bexram33@mail.ru'], fail_silently=False)
-        super().save(*args, **kwargs)
-
     class Meta:
         verbose_name_plural = 'Заявки-обращения'
         verbose_name = 'Заявка-обращение'
+
+
+@receiver(post_save, sender=Crm_forms)
+def add_form(sender, instance, created, **kwargs):
+    if created:
+        email = EmailMessage(
+            'Заявка ' + str(instance.type) + ' ' + str(datetime.datetime.now()),
+            body=str(instance.name) + ' ' + str(instance.telephone) + ' ' + str(instance.email) + ' ' + str(
+                instance.other),
+            from_email=settings.EMAIL_HOST_USER,
+            to=('ivan@adving.ru', 'bexram33@mail.ru'),
+        )
+        if instance.file:
+            email.attach_file(instance.file.path)
+        email.send()
