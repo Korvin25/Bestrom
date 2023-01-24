1cd7a6e6f3b154998fad357ea28a1593e71c9c82
antonkostin <antonkostin86@gmail.com>
2023-01-24 22:37:27 +0300

    fix

--- bestrom_api/config/urls.py
+++ bestrom_api/config/urls.py
@@ -21,13 +21,13 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('', TemplateView.as_view(template_name='templates/index.html'), name='index'),
-    path('', include('content.urls')),
-    path('', include('product.urls')),
-    path('', include('crm.urls'))
+   #  path('', TemplateView.as_view(template_name='templates/index.html'), name='index'),
+    path('api/', include('content.urls')),
+    path('api/', include('product.urls')),
+    path('api/', include('crm.urls'))
 ]
