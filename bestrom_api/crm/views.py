5ae9da11b1786cc49b708f5bf4600a208fcf774c
soyus1 <soyus.s1@yandex.ru>
2025-02-13 19:18:06 +0300

    test fix form

--- bestrom_api/crm/views.py
+++ bestrom_api/crm/views.py
@@ -1,15 +1,14 @@
-from rest_framework import status, mixins, generics
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
+from django.views.decorators.csrf import csrf_exempt
 from .models import Crm_forms
 from .serializers import CrmFormsSerializer
 
-
+@csrf_exempt
 class FormsList(generics.ListCreateAPIView):
     queryset = Crm_forms.objects.all()
     serializer_class = CrmFormsSerializer
 
-
+@csrf_exempt
 class FormsDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Crm_forms.objects.all()
     serializer_class = CrmFormsSerializer
